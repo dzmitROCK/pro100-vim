9ecf5027c46447af1a5df89302bbbe51e0243066
dzmitrock <besfamilny87@yandex.ru>
2017-10-14 14:28:24 +0300

    first commit

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FOLDER_VIM_AWESOME="$HOME/.vim_runtime"
+
+
+if [ -d $FOLDER_VIM_AWESOME ]
+then
+    echo "I find vim awesome folder. Delete[y/n]?"
+    read DEL_VIM_AWESOME
+
+	if [ $DEL_VIM_AWESOME = "y" ]
+		then
+	        rm -rf $FOLDER_VIM_AWESOME
+	        git clone --depth=1 https://github.com/amix/vimrc.git $HOME/.vim_runtime
+	        sh $HOME/.vim_runtime/install_awesome_vimrc.sh
+	    else exit 1
+	fi
+fi
+
