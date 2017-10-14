426c244ddb6e1beb9b70a9a39815276bd1273123
dzmitrock <besfamilny87@yandex.ru>
2017-10-14 15:17:40 +0300

    ..

--- test.sh
+++ test.sh
@@ -5,15 +5,18 @@ FOLDER_VIM_AWESOME="$HOME/.vim_runtime"
 
 if [ -d $FOLDER_VIM_AWESOME ]
 then
-    echo "I find vim awesome folder. Delete[y/n]?"
+    echo "Вай вай... А я нашел установленный The Ultimate vimrc.. Удалить и заново преустановить[y/n]?"
     read DEL_VIM_AWESOME
 
 	if [ $DEL_VIM_AWESOME = "y" ]
-		then
-	        rm -rf $FOLDER_VIM_AWESOME
-	        git clone --depth=1 https://github.com/amix/vimrc.git $HOME/.vim_runtime
-	        sh $HOME/.vim_runtime/install_awesome_vimrc.sh
-	    else exit 1
+	then
+        rm -rf $FOLDER_VIM_AWESOME
+        git clone --depth=1 https://github.com/amix/vimrc.git $HOME/.vim_runtime
+        sh $HOME/.vim_runtime/install_awesome_vimrc.sh
+	else
+        exit 1
 	fi
 fi
 
+echo "\"include pro100-vim config
+source $HOME/.pro100-vim/.vimrc" >> $HOME/.vimrc
