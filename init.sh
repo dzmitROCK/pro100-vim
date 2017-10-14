0f4c4a24082a1d5a6a69ad1bf59e98a9c75a9f39
dzmitrock <besfamilny87@yandex.ru>
2017-10-14 16:46:32 +0300

    ..

--- init.sh
+++ init.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 
-FOLDER_VIM_AWESOME="$HOME/.vim_runtime"
+NAME_ULTIMATE_VIMRC="The Ultimate vimrc"
+ULTIMATE_VIMRC="$HOME/.vim_runtime"
 
-if [ -d $FOLDER_VIM_AWESOME ]
+# проверка на существование каталога The Ultimate vimrc
+if [ -d $ULTIMATE_VIMRC ]
+# если нашли то жалуемся
 then
-    echo "Вай вай... А я нашел установленный The Ultimate vimrc.. Удалить и заново преустановить?[y/n]"
+    echo "Вай вай... А я нашел установленный $NAME_ULTIMATE_VIMRC.."
+    echo "Удалить и заново преустановить $NAME_ULTIMATE_VIMRC?[y/n]"
+
+    # спрашиваем о переустановке The Ultimate vimrc
     read DEL_VIM_AWESOME
 
+    # если ответ y
 	if [ $DEL_VIM_AWESOME = "y" ]
+    # то удаляем папку, клонируем репозиторий и запускаем скрипт
 	then
-        rm -rf $FOLDER_VIM_AWESOME
+        rm -rf $ULTIMATE_VIMRC
         git clone --depth=1 https://github.com/amix/vimrc.git $HOME/.vim_runtime
         sh $HOME/.vim_runtime/install_awesome_vimrc.sh
     else
         exit 1
     fi
-else 
+# если не нашли то клонируем и запускаем
+else
     git clone --depth=1 https://github.com/amix/vimrc.git $HOME/.vim_runtime
     sh $HOME/.vim_runtime/install_awesome_vimrc.sh
 fi
 
+# дописываем в .vimrc строчку подключения pro100-vim
 echo "
-\"include pro100-vim config
+\"подключаем pro100-vim config
 source ~/.pro100-vim/.vimrc" >> $HOME/.vimrc
+
+# вот и сказочке конец
+echo "Всё успешно завершено"
